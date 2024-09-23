666942543016baa177157b3a31aa66e5aaa62c7b
Abel <lujanlom0120@gmail.com>
2024-09-23 17:10:47 -0600

    tailwind

---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+"use client";
+import '../src/index.css';
+function Login() {
+    return (
+        <div className="bg-black w-full h-full">
+            <h1 className="bg-red-500 text-6xl">LOGIN</h1>
+            <button type='button' className='bg-red-100'>Iniciar Sesion</button>
+        </div>
+    );
+}
+export default Login;
